d67b491f98136535e07646182ab583cbb95630d1
Michael Wong <michael.brian.wong@gmail.com>
2015-11-02 22:14:28 -0700

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers,     only[:show, :index]
-      resources :invoices,      only[:show, :index]
-      resources :invoice_items, only[:show, :index]
-      resources :items,         only[:show, :index]
-      resources :merchants,     only[:show, :index]
-      resources :transactions,  only[:show, :index]
+      resources :customers,     only: [:show, :index]
+      resources :invoices,      only: [:show, :index]
+      resources :invoice_items, only: [:show, :index]
+      resources :items,         only: [:show, :index]
+      resources :merchants,     only: [:show, :index]
+      resources :transactions,  only: [:show, :index]
     end
   end
 end
