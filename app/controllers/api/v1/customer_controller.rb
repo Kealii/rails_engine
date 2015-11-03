26ba5c1269536a2d82d3e2eaebd919e33ab7bbfb
Michael Wong <michael.brian.wong@gmail.com>
2015-11-03 06:18:47 -0700

    adds customer controller with index and show actions

---
+++ app/controllers/api/v1/customer_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::CustomerController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
-      resources :customers,     only: [:show, :index]
-      resources :invoices,      only: [:show, :index]
-      resources :invoice_items, only: [:show, :index]
-      resources :items,         only: [:show, :index]
-      resources :merchants,     only: [:show, :index]
-      resources :transactions,  only: [:show, :index]
+      resources :customers,     default: { format: :json }, only: [:show, :index]
+      resources :invoices,      default: { format: :json }, only: [:show, :index]
+      resources :invoice_items, default: { format: :json }, only: [:show, :index]
+      resources :items,         default: { format: :json }, only: [:show, :index]
+      resources :merchants,     default: { format: :json }, only: [:show, :index]
+      resources :transactions,  default: { format: :json }, only: [:show, :index]
     end
   end
 end
