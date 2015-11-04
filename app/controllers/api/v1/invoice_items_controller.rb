b815b61cccabdef05b81d1724b082947657d1004
Michael Wong <michael.brian.wong@gmail.com>
2015-11-03 19:55:43 -0700

    adds test for find and find all for invoice items controller

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -6,4 +6,18 @@ class Api::V1::InvoiceItemsController < ApplicationController
   def index
     respond_with InvoiceItem.all
   end
+
+  def find
+    respond_with InvoiceItem.find_by(find_params)
+  end
+
+  def find_all
+    respond_with InvoiceItem.where(find_params)
+  end
+
+  private
+
+  def find_params
+    params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price)
+  end
 end
--- spec/controllers/api/v1/invoice_items_controller_spec.rb
+++ spec/controllers/api/v1/invoice_items_controller_spec.rb
@@ -2,35 +2,125 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
 
-  let(:invoice_item) { InvoiceItem.create(item_id: 1,
-                                         invoice_id: 2,
-                                         quantity: 3,
-                                         unit_price: 4) }
+  let!(:invoice_item1) { InvoiceItem.create(item_id:   1,
+                                           invoice_id: 2,
+                                           quantity:   3,
+                                           unit_price: 4) }
+
+  let!(:invoice_item2) { InvoiceItem.create(item_id:   1,
+                                           invoice_id: 2,
+                                           quantity:   3,
+                                           unit_price: 4) }
+
+  let!(:invoice_item3) { InvoiceItem.create(item_id:   5,
+                                           invoice_id: 6,
+                                           quantity:   7,
+                                           unit_price: 8) }
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show, id: invoice_item.id, format: :json
-      expect(response).to have_http_status(:success)
-    end
-
     it "returns the correct invoice item" do
-      get :show, id: invoice_item.id, format: :json
-      expect(json_response["quantity"]).to eq invoice_item.quantity
-      expect(json_response["unit_price"].to_f).to eq invoice_item.unit_price
+      get :show, id: invoice_item1.id, format: :json
 
+      expect(response).to have_http_status(:success)
+      expect(json_response["quantity"]).to eq invoice_item1.quantity
+      expect(json_response["unit_price"].to_f).to eq invoice_item1.unit_price
     end
   end
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index, format: :json
-      expect(response).to have_http_status(:success)
-    end
-
     it "returns the right number of invoice items" do
       number_of_invoice_items = InvoiceItem.count
       get :index, format: :json
+
+      expect(response).to have_http_status(:success)
       expect(json_response.count).to eq number_of_invoice_items
+    end
+  end
+
+  describe "GET #find" do
+    it "returns the correct invoice item by id" do
+      get :find, id: invoice_item1.id, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response["item_id"]).to eq invoice_item1.item_id
+      expect(json_response["quantity"]).to eq invoice_item1.quantity
+    end
+
+    it "returns the correct invoice item by item id" do
+      get :find, item_id: invoice_item1.item_id, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response["quantity"]).to eq invoice_item1.quantity
+      expect(json_response["id"]).to eq invoice_item1.id
+    end
+
+    it "returns the correct invoice_item by invoice id" do
+      get :find, invoice_id: invoice_item1.invoice_id, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response["quantity"]).to eq invoice_item1.quantity
+      expect(json_response["id"]).to eq invoice_item1.id
+    end
+
+    it "returns the correct invoice_item by quantity" do
+      get :find, quantity: invoice_item1.quantity, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response["quantity"]).to eq invoice_item1.quantity
+    end
+
+    it "returns the correct_invoice item by unit price" do
+      get :find, unit_price: invoice_item1.unit_price, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response["unit_price"].to_f).to eq invoice_item1.unit_price
+    end
+  end
+
+  describe "GET #find_all" do
+    it "returns the correct invoice items by id" do
+      get :find_all, id: invoice_item1.id, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response.count).to eq 1
+      expect(json_response.first["item_id"]).to eq invoice_item1.item_id
+      expect(json_response.first["quantity"]).to eq invoice_item1.quantity
+    end
+
+    it "returns the correct invoice items by item id" do
+      get :find_all, item_id: invoice_item1.item_id, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response.count).to eq 2
+      expect(json_response.first["item_id"]).to eq invoice_item1.item_id
+      expect(json_response.last["item_id"]).to eq invoice_item1.item_id
+    end
+
+    it "returns the correct invoice_items by invoice id" do
+      get :find_all, invoice_id: invoice_item1.invoice_id, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response.count).to eq 2
+      expect(json_response.first["invoice_id"]).to eq invoice_item1.invoice_id
+      expect(json_response.last["invoice_id"]).to eq invoice_item1.invoice_id
+    end
+
+    it "returns the correct invoice_items by quantity" do
+      get :find_all, quantity: invoice_item1.quantity, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response.count).to eq 2
+      expect(json_response.first["quantity"]).to eq invoice_item1.quantity
+      expect(json_response.last["quantity"]).to eq invoice_item1.quantity
+    end
+
+    it "returns the correct_invoice items by unit price" do
+      get :find_all, unit_price: invoice_item1.unit_price, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response.count).to eq 2
+      expect(json_response.first["unit_price"].to_f).to eq invoice_item1.unit_price
+      expect(json_response.last["unit_price"].to_f).to eq invoice_item1.unit_price
     end
   end
 end
