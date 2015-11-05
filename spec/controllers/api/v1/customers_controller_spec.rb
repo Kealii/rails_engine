89b8bf9a8742070aacc8ee9666cb3a6b141d1b7c
Michael Wong <michael.brian.wong@gmail.com>
2015-11-04 21:53:27 -0700

    fixes test for transactions

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Api::V1::CustomersController, type: :controller do
 
   let!(:transaction1) { Transaction.create(invoice_id: invoice1.id,
                                            result:     "success") }
+  let!(:transaction2) { Transaction.create(invoice_id: invoice2.id,
+                                           result:     "success") }
+  let!(:transaction3) { Transaction.create(invoice_id: invoice3.id,
+                                           result:     "failed") }
 
   let!(:invoice1) { Invoice.create(customer_id: customer1.id,
                                    merchant_id: merchant1.id,
@@ -114,6 +118,17 @@ RSpec.describe Api::V1::CustomersController, type: :controller do
       expect(json_response.count).to eq 2
       expect(json_response.first["id"]).to eq invoice1.id
       expect(json_response.last["id"]).to eq invoice2.id
+    end
+  end
+
+  describe "GET #transactions" do
+    it "returns transactions for a customer" do
+      get :transactions, customer_id: customer1.id, format: :json
+
+      expect(response).to have_http_status :success
+      expect(json_response.count).to eq 2
+      expect(json_response.first["id"]).to eq transaction1.id
+      expect(json_response.last["id"]).to eq transaction2.id
     end
   end
 
