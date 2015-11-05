8c9f9059e2bde6876664aef6803363d2a7bd008a
Michael Wong <michael.brian.wong@gmail.com>
2015-11-04 21:50:08 -0700

    adds transactions action to customer controller

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,7 @@ class Customer < ActiveRecord::Base
   has_many :transactions, through: :invoices
 
   def favorite_merchant
-    successful_invoices = invoices.successful.group_by(&:merchant_id)
-    sorted_invoices = successful_invoices.sort_by { |_, v| v.count }.reverse.flatten
-    id = sorted_invoices.first
+    id = invoices.successful.group_by(&:merchant_id).sort_by { |k, v| v.count }.reverse.flatten.first
     Merchant.find(id)
   end
 end
